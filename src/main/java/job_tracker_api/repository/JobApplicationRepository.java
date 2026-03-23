e45190a69dd73f2f14e01cca79132755b54fcb63
Mahadevj21 <mahadevjangid21@gmail.com>
2026-03-23 20:15:36 +0530

    feat: add JobApplication repository

---
+++ src/main/java/job_tracker_api/repository/JobApplicationRepository.java
@@ -0,0 +1,9 @@
+package job_tracker_api.repository;
+
+import job_tracker_api.model.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+}
