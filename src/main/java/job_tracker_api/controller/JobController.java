80602d3a4545d2a34683ba6435e2d45c048f50ac
Mahadevj21 <mahadevjangid21@gmail.com>
2026-03-25 07:48:24 +0530

    feat: create JobController

---
+++ src/main/java/job_tracker_api/controller/JobController.java
@@ -0,0 +1,15 @@
+package job_tracker_api.controller;
+
+import job_tracker_api.service.JobService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jobs")
+@RequiredArgsConstructor
+public class JobController {
+
+    private final JobService jobService;
+
+}
