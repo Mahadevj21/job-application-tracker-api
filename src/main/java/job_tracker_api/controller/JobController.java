93221abf0d1ea19fb7492758239d92ffcb0d20c9
Mahadevj21 <mahadevjangid21@gmail.com>
2026-03-25 07:53:36 +0530

    feat: implement job creation and retrieval controller

--- src/main/java/job_tracker_api/controller/JobController.java
+++ src/main/java/job_tracker_api/controller/JobController.java
@@ -1,9 +1,13 @@
 package job_tracker_api.controller;
 
+import job_tracker_api.model.JobApplication;
 import job_tracker_api.service.JobService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/jobs")
@@ -12,4 +16,19 @@ public class JobController {
 
     private final JobService jobService;
 
+    @PostMapping
+    public ResponseEntity<JobApplication> createJob(@RequestBody JobApplication jobApplication) {
+        JobApplication createdJob = jobService.createJob(jobApplication);
+        return new ResponseEntity<>(createdJob, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JobApplication>> getAllJobs() {
+        return ResponseEntity.ok(jobService.getAllJobs());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobApplication> getJobById(@PathVariable Long id) {
+        return ResponseEntity.ok(jobService.getJobById(id));
+    }
 }
