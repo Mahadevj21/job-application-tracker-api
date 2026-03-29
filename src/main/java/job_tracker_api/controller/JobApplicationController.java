6ab046a55f336361a4ebdf802051272692b16ec6
Mahadevj21 <mahadevjangid21@gmail.com>
2026-03-29 22:06:08 +0530

    feat: add JobApplicationController with basic CRUD

--- src/main/java/job_tracker_api/controller/JobApplicationController.java
+++ src/main/java/job_tracker_api/controller/JobApplicationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/jobs")
 @RequiredArgsConstructor
-public class JobController {
+public class JobApplicationController {
 
     private final JobApplicationService jobApplicationService;
 
